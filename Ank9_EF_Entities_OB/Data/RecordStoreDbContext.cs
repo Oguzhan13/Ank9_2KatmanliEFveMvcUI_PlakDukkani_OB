e013ada5980b1bd0ae8afe1c1389bcd34b7d34c0
Oguzhan13 <oguzhanbayram13@gmail.com>
2022-11-03 10:45:24 +0300

    ilk güncelleme

---
+++ Ank9_EF_Entities_OB/Data/RecordStoreDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Ank9_EF_Entities_OB.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ank9_EF_Entities_OB.Data
+{
+    public class RecordStoreDbContext:DbContext
+    {
+        public RecordStoreDbContext(DbContextOptions<RecordStoreDbContext> options):base(options)
+        {
+
+        }
+
+        ////class içerisinde yaptığımız Index attribute unun yaptığı işlemi buradaki gibi yapabiliriz 
+        ////User.cs -> satır 11-12 aralığında
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<User>()
+        //        .HasIndex(x => x.Name).IsUnique();
+        //}
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Ank9_EF_Entities_OB/Model/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ank9_EF_Entities_OB.Model
+{
+    ////Index nameof ile belirttiğimiz property içerisinde IsUnique ile aynı isimden tekrar girilememesini sağlıyoruz
+    //[Index(nameof(Name), IsUnique = true)]
+    public class User
+    {
+        //[Key]
+        public int UserId { get; set; }
+        //[StringLength(30,MinimumLength =3, ErrorMessage ="Karakter sayısı 3 ile 30 aralığında olmalıdır")]
+                   
+        public string Name { get; set; }
+        //[StringLength(8,MinimumLength = 4,ErrorMessage ="Karakter sayısı 4 ile 8 aralığında olmalıdır")]
+        public string Password { get; set; }
+    }
+}
